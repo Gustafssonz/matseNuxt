c446c2f240e6ee93044b393672b1b7413c40c585
Niklas Gustafsson <niklas.m.gustafsson@gmail.com>
2021-04-14 16:39:58 +0200

    2 filters soltuons, some issues with filtering when you change category etc

--- components/Result.vue
+++ components/Result.vue
@@ -1,11 +1,10 @@
 <template>
 	<div>
-		<div v-if="dataProps" class="searchbox">
+		<div v-if="dataProps.length > 1" class="searchbox">
 			<!-- <Searchbox :productList="data" /> -->
 			<!-- @click="filterObject" -->
 			<!-- <input v-model="message" placeholder="edit me"> -->
 			<v-text-field v-model="msg"> </v-text-field>
-			<v-btn elevation="2" @click="refresh">{{ msg }}</v-btn>
 		</div>
 		<div class="productsbox">
 			<Product
@@ -22,28 +21,32 @@ export default {
 	props: ["dataProps"],
 	data() {
 		return {
-			msg: '',
+			msg: "",
 		};
 	},
 	computed: {
 		filteredProducts: function () {
-			if (this.msg == '') {
+			if (this.msg == "") {
 				return this.dataProps;
 			} else {
-				var newArray = [];
-                this.dataProps.filter((product) => {
-                    if(product.name.toLowerCase().includes(this.msg.toLowerCase())){
-                    newArray.push(product);
-                    }
+                // Solution 1, not working
+				/* return this.dataProps.filter((product) => {
+					product.name.toLowerCase().includes(this.msg.toLowerCase());
+				}); */
+                // Solution 2, working.
+                var newArray = []
+				this.dataProps.filter((product) => {
+					if (product.name.toLowerCase().includes(this.msg.toLowerCase())) {
+						newArray.push(product);
+					}
 				});
-                console.log(newArray);
-                return newArray;
+				return newArray;
 			}
 		},
 	},
 	methods: {
 		refresh() {
-            console.log(this.dataProps)
+			console.log(this.dataProps);
 		},
 	},
 };
@@ -54,6 +57,6 @@ export default {
 	display: flex;
 	align-content: flex-start;
 	/* flex-direction: row; */
-	/* flex-wrap: wrap; */
+	flex-wrap: wrap;
 }
 </style>
